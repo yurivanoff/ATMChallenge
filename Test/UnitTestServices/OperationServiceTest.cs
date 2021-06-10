0f4dd854d2e7b9794ea666527a71430f99644f20
Yuri Ivanoff <yuri.ivanoff@guide.com.br>
2021-06-10 12:12:41 -0300

    tests: implementar segundo cenario de testes

--- Test/UnitTestServices/OperationServiceTest.cs
+++ Test/UnitTestServices/OperationServiceTest.cs
@@ -18,7 +18,7 @@ namespace Test.UnitTestServices
         }
 
         [Fact(DisplayName = "TESTE 1 - NOTAS DISPONIVEIS: [100, 50, 20] - Saque 150")]
-        public void WithdrawOneHundredAndFiftySuccess()
+        public void WithdrawFirstTestOneHundredAndFiftySuccess()
         {
             _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20 });
 
@@ -30,7 +30,7 @@ namespace Test.UnitTestServices
         }
 
         [Fact(DisplayName = "TESTE 1 - NOTAS DISPONIVEIS: [100, 50, 20] - Saque 120")]
-        public void WithdrawOneHundredAndTwentySuccess()
+        public void WithdrawFirstTestOneHundredAndTwentySuccess()
         {
             _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20 });
 
@@ -42,7 +42,7 @@ namespace Test.UnitTestServices
         }
 
         [Fact(DisplayName = "TESTE 1 - NOTAS DISPONIVEIS: [100, 50, 20] - Saque 170")]
-        public void WithdrawOneHundredAndSeventySuccess()
+        public void WithdrawFirstTestOneHundredAndSeventySuccess()
         {
             _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20 });
 
@@ -55,7 +55,7 @@ namespace Test.UnitTestServices
         }
 
         [Fact(DisplayName = "TESTE 1 - NOTAS DISPONIVEIS: [100, 50, 20] - Saque 1")]
-        public void WithdrawOneError()
+        public void WithdrawFirstTestOneError()
         {
             _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20 });
 
@@ -65,7 +65,7 @@ namespace Test.UnitTestServices
         }
 
         [Fact(DisplayName = "TESTE 1 - NOTAS DISPONIVEIS: [100, 50, 20] - Saque 5")]
-        public void WithdrawFiveError()
+        public void WithdrawFirstTestFiveError()
         {
             _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20 });
 
@@ -75,13 +75,62 @@ namespace Test.UnitTestServices
         }
 
         [Fact(DisplayName = "TESTE 1 - NOTAS DISPONIVEIS: [100, 50, 20] - Saque 130")]
-        public void WithdrawOneHundredAndThirtyError()
+        public void WithdrawFirstTestOneHundredAndThirtyError()
         {
             _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20 });
 
             var operationService = new OperationService(_machineCashRepositoryMock.Object);
 
             Assert.ThrowsAny<Exception>(() => operationService.Withdraw(130));
+        }
+
+        [Fact(DisplayName = "TESTE 2 - NOTAS DISPONIVEIS: [100, 50, 20, 10, 5] - Saque 175")]
+        public void WithdrawSecondTestOneHundredAndSeventyFiveSuccess()
+        {
+            _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20, 10, 5 });
+
+            var operationService = new OperationService(_machineCashRepositoryMock.Object);
+            var withdrawResult = operationService.Withdraw(175);
+
+            Assert.True(withdrawResult.First(x => x.Key == 100).Value == 1);
+            Assert.True(withdrawResult.First(x => x.Key == 50).Value == 1);
+            Assert.True(withdrawResult.First(x => x.Key == 20).Value == 1);
+            Assert.True(withdrawResult.First(x => x.Key == 5).Value == 1);
+        }
+
+        [Fact(DisplayName = "TESTE 2 - NOTAS DISPONIVEIS: [100, 50, 20, 10, 5] - Saque 110")]
+        public void WithdrawSecondTestOneHundredAndTenSuccess()
+        {
+            _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20, 10, 5 });
+
+            var operationService = new OperationService(_machineCashRepositoryMock.Object);
+            var withdrawResult = operationService.Withdraw(110);
+
+            Assert.True(withdrawResult.First(x => x.Key == 100).Value == 1);
+            Assert.True(withdrawResult.First(x => x.Key == 10).Value == 1);
+        }
+
+        [Fact(DisplayName = "TESTE 2 - NOTAS DISPONIVEIS: [100, 50, 20, 10, 5] - Saque 75")]
+        public void WithdrawSeventyFiveSuccess()
+        {
+            _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20, 10, 5 });
+
+            var operationService = new OperationService(_machineCashRepositoryMock.Object);
+            var withdrawResult = operationService.Withdraw(75);
+
+            Assert.True(withdrawResult.First(x => x.Key == 50).Value == 1);
+            Assert.True(withdrawResult.First(x => x.Key == 20).Value == 1);
+            Assert.True(withdrawResult.First(x => x.Key == 5).Value == 1);
+        }
+
+        [Fact(DisplayName = "TESTE 2 - NOTAS DISPONIVEIS: [100, 50, 20, 10, 5] - Saque 42")]
+        public void WithdrawFortyTwoError()
+        {
+            _machineCashRepositoryMock.Setup(x => x.GetValues()).Returns(() => new List<int> { 100, 50, 20, 10, 5 });
+
+            var operationService = new OperationService(_machineCashRepositoryMock.Object);
+
+            Assert.ThrowsAny<Exception>(() => operationService.Withdraw(42));
         }
     }
 }
